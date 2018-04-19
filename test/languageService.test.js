27aa55b0f7695bbaad727005bce26fd7d6f91c8d
Macku Sanchez <mackusanchez@gmail.com>
2018-04-19 15:01:31 +0800

    JS format for testing

--- test/languageService.test.js
+++ test/languageService.test.js
@@ -8,25 +8,86 @@ var details = {
     dateExecuted: new Date(),
 }
 
-console.log('Author: ' + details.author);
-console.log('Date Executed: ' + details.dateExecuted);
+function testAuthor(){
+    console.log('   Author: ' + details.author);
+    console.log('   Date Executed: ' + details.dateExecuted+'\n');
+}
 
-before(function(){
-    //set timeout to 10s
-    this.timeout(10000);
-});
 
 //the test cases only check return values from the service (promise). check db values manually
-describe('getDefaultLanguage()', function(){
 
-    //it is suggested to clean (either manually or other service methods) the test data inserted to the database
-    //if service methods will be used, use before() hook
-    //before(function(){});
 
-    it('should return an object that has a language pack that depends on the default language set by the Admin', function(){
-        return languageService.getDefaultLanguage({}).then(function(data){
-            expect(data).to.be.an('object');
+describe('language service testing', function(){
+
+   before(function(){
+        //set timeout to 10s
+        this.timeout(10000);
+        testAuthor();
+    });
+
+    describe('getDefaultLanguage()', function(){
+
+        
+
+        //it is suggested to clean (either manually or other service methods) the test data inserted to the database
+        //if service methods will be used, use before() hook
+        //before(function(){});
+
+        it('should return an object that has a language pack that depends on the default language set by the Admin', function(){
+            //testAuthor();
+            return languageService.getDefaultLanguage({}).then(function(data){
+                //console.log(Object.keys(data)[0]);
+                expect(data).to.be.an('object');
+            });
         });
+        
+    });
+
+    describe('getSpecificLanguage()', function(){
+         
+        //it is suggested to clean (either manually or other service methods) the test data inserted to the database
+        //if service methods will be used, use before() hook
+        //before(function(){});
+
+        it('should return an object that is english language pack', function(){
+            //testAuthor();
+            var qwe={params:{user:'english'}};
+        
+            return languageService.getSpecificLanguage(qwe).then(function(data){
+                //console.log(Object.keys(data)[0]);
+                expect(data).to.have.property('English');
+            });
+        });
+
+        it('should return an error', function(){
+            var qwe={params:{user:'nihongo'}};
+        
+            return languageService.getSpecificLanguage(qwe).then(function(data){
+                //console.log(Object.keys(data)[0]);
+            }).catch(function(error){
+                expect(data).to.have.property('English');
+            })
+        });
+
+        it('should return an object that is 日本語 language pack', function(){
+            var qwe={params:{user:'nihongo'}};
+        
+            return languageService.getSpecificLanguage(qwe).then(function(data){
+                //console.log(Object.keys(data)[0]);
+                expect(data).to.have.property('日本語');
+            });
+        });
+        it('should return an error', function(){
+            var qwe={params:{user:'日本語'}};
+        
+            return languageService.getSpecificLanguage(qwe).then(function(data){
+                //console.log(Object.keys(data)[0]);
+            }).catch(function(error){
+                expect(data).to.have.property('日本語');
+            })
+        });
+        
     });
     
 });
+
